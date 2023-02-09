7187105165ef8494ccb23c5a614bc0159b85c786
Iryna Klimkova <irraris21@gmail.com>
2023-02-09 18:25:06 +0400

    Change tweet_job.rb

--- app/jobs/tweet_job.rb
+++ app/jobs/tweet_job.rb
@@ -5,7 +5,7 @@ class TweetJob < ApplicationJob
     # binding.irb
     return if tweet.published?
     
-    return if tweet.publish_at > Time.current 
+    return if tweet.publish_at > Time.zone.now
 
     tweet.publish_to_twitter!
   end
